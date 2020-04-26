acda626121e28fe67f81353f4fd676a3fa24855b
Fabien Kérinec <fabien.kerinec@gmail.com>
2020-04-26 18:49:03 +0200

    server heroku

---
+++ server.js
@@ -0,0 +1,26 @@
+/** ************************************************************************
+IMPORTS
+************************************************************************* */
+
+const express = require('express');
+const history = require('connect-history-api-fallback');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+/** ************************************************************************
+EXPRESS SERVER
+************************************************************************* */
+
+const app = express();
+app.use(
+  history({
+    verbose: true
+  })
+);
+app.use(serveStatic(path.join(__dirname, '/dist')));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log('Server started at http://localhost:5000');
+});
